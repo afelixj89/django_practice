cfd572937243a6fc5970edc270a5adda203d41d3
Antonio <afelixj89@gmail.com>
2024-04-04 10:30:27 -0400

    Added auth

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Home, DogList, DogDetail, FeedingListCreate, FeedingDetail , ToyListCreate, ToyDetail, AddToyToDog, RemoveToyFromDog
+from .views import Home, DogList, DogDetail, FeedingListCreate, FeedingDetail , ToyListCreate, ToyDetail, AddToyToDog, RemoveToyFromDog, CreateUserView, LoginView, VerifyUserView
 
 urlpatterns = [
   path('', Home.as_view(), name='home'),
@@ -11,6 +11,9 @@ urlpatterns = [
 	path('dogs/<int:dog_id>/toys/<int:id>/', ToyDetail.as_view(), name='toy-detail'),
   path('dogs/<int:dog_id>/dog_toy/<int:toy_id>/', AddToyToDog.as_view(), name='add-toy-to-dog'),
   path('dogs/<int:dog_id>/remove_toy/<int:toy_id>/', RemoveToyFromDog.as_view(), name='remove-toy-from-dog'),
+  path('users/register/', CreateUserView.as_view(), name='register'),
+  path('users/login/', LoginView.as_view(), name='login'),
+  path('users/token/refresh/', VerifyUserView.as_view(), name='token_refresh')
 
 ]
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,24 +1,85 @@
 from django.shortcuts import render
+from rest_framework.exceptions import PermissionDenied 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Dog, Feeding, Toy
-from .serializers import DogSerializer, FeedingSerializer, ToySerializer
-from rest_framework import generics
+from .serializers import UserSerializer,DogSerializer, FeedingSerializer, ToySerializer
+from rest_framework import generics, status, permissions
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 
-# Define the home view
+class CreateUserView(generics.CreateAPIView):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
+
+  def create(self, request, *args, **kwargs):
+    response = super().create(request, *args, **kwargs)
+    user = User.objects.get(username=response.data['username'])
+    refresh = RefreshToken.for_user(user)
+    return Response({
+      'refresh': str(refresh),
+      'access': str(refresh.access_token),
+      'user': response.data
+    })
+
+# User Login
+class LoginView(APIView):
+  permission_classes = [permissions.AllowAny]
+
+  def post(self, request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    user = authenticate(username=username, password=password)
+    if user:
+      refresh = RefreshToken.for_user(user)
+      return Response({
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+        'user': UserSerializer(user).data
+      })
+    return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+# User Verification
+class VerifyUserView(APIView):
+  permission_classes = [permissions.IsAuthenticated]
+
+  def get(self, request):
+    user = User.objects.get(username=request.user)  # Fetch user profile
+    refresh = RefreshToken.for_user(request.user)  # Generate new refresh token
+    return Response({
+      'refresh': str(refresh),
+      'access': str(refresh.access_token),
+      'user': UserSerializer(user).data
+    })
+
+
+
 class Home(APIView):
   def get(self, request):
     content = {'message': 'Welcome to the dog-collector api home route!'}
     return Response(content)
   
 class DogList(generics.ListCreateAPIView):
-  queryset = Dog.objects.all()
+  permission_classes = [permissions.IsAuthenticated]
   serializer_class = DogSerializer
 
+  def get_queryset(self):
+      # This ensures we only return cats belonging to the logged-in user
+      user = self.request.user
+      return Dog.objects.filter(user=user)
+
+  def perform_create(self, serializer):
+      # This associates the newly created cat with the logged-in user
+      serializer.save(user=self.request.user)
+
 class DogDetail(generics.RetrieveUpdateDestroyAPIView):
-  queryset = Dog.objects.all()
   serializer_class = DogSerializer
   lookup_field = 'id'
+
+  def get_queryset(self):
+    user = self.request.user
+    return Dog.objects.filter(user=user)
 
 
   def retrieve(self, request, *args, **kwargs):
@@ -32,6 +93,27 @@ class DogDetail(generics.RetrieveUpdateDestroyAPIView):
         'dog': serializer.data,
         'toys_not_associated': toys_serializer.data
     })
+  
+  def perform_update(self, serializer):
+    dog = self.get_object()
+    if dog.user != self.request.user:
+        raise PermissionDenied({"message": "You do not have permission to edit this dog."})
+    serializer.save()
+
+  def perform_destroy(self, instance):
+    if instance.user != self.request.user:
+        raise PermissionDenied({"message": "You do not have permission to delete this dog."})
+    instance.delete()
+  
+
+
+
+
+
+
+
+
+
 
 
 class FeedingListCreate(generics.ListCreateAPIView):
